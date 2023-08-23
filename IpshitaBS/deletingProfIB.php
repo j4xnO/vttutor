3cd7582c09bf194112057a4a4fcb87460e2f19ee
j4xnO <143028360+j4xnO@users.noreply.github.com>
2023-08-23 14:54:45 -0400

    Add files via upload

---
+++ IpshitaBS/deletingProfIB.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>deleting prof</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+    <section class="py-4 py-xl-5"></section>
+    <div class="text-white bg-dark border rounded border-0 p-4 p-md-5">
+        <h2 class="fw-bold text-white mb-3">Delete Profile?</h2>
+        <p class="mb-4"></p>
+        <div class="my-3"><a class="btn btn-primary btn-lg me-2" role="button" href="#">Yes</a><a class="btn btn-light btn-lg" role="button" href="#">No</a></div>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ IpshitaBS/modifyAnddeleteIB.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>modify &amp; delete</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+</head>
+
+<body>
+    <section class="position-relative py-4 py-xl-5">
+        <div class="container position-relative">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+                    <div class="card mb-5">
+                        <div class="card-body p-sm-5"><img src="assets/img/vt%20tutorR.png">
+                            <h2 class="text-center mb-4">Profile</h2>
+                            <form method="post">
+                                <div class="mb-3"></div>
+                                <div class="mb-3"></div><input class="form-control" type="text" id="name-2" name="name" placeholder="Profile Type: Student"><input class="form-control" type="text" id="name-1" name="name" placeholder="Name: John Doe"><input class="form-control" type="email" id="email-2" name="email" placeholder="Email: johndoe@vt.edu"><input class="form-control" type="password" name="Password" placeholder="Password: *********">
+                                <div class="mb-3"><textarea class="form-control" id="message-2" name="List of Classes:" rows="6" placeholder="List of Classes: BIT 4444, BIT 1234"></textarea></div>
+                                <div></div>
+                            </form><button class="btn btn-primary" type="submit">&nbsp; &nbsp;Modify&nbsp; &nbsp;</button><button class="btn btn-primary" type="button" style="margin-right: 1px;margin-left: 146px;padding-top: 8px;">Delete&nbsp;</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
